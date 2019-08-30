30e6d4a39a0284f05d76c5ee5811c691d2aec6d8
Rafael Audy Glanzner <rafael.audy@gmail.com>
2019-08-30 10:16:19 +0100

    Sets up jest

--- other/configuration/calculator/.babelrc.js
+++ other/configuration/calculator/.babelrc.js
@@ -1,84 +1,10 @@
+const isTest = String(process.env.NODE_ENV) === 'test'
 module.exports = {
-  presets: [['env', {modules: false}], 'react'],
+  presets: [['env', {modules: isTest ? 'commonjs' : false}], 'react'],
   plugins: [
     'syntax-dynamic-import',
     'transform-class-properties',
     'transform-object-rest-spread',
-  ],
+    isTest ? 'dynamic-import-node' : null,
+  ].filter(Boolean),
 }
-
-/*
-Solution snippets below
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const isTest = String(process.env.NODE_ENV) === 'test'
-
-
-for the env plugin modules config:
-isTest ? 'commonjs' : false
-
-For dynamic import config in plugins
-isTest ? 'dynamic-import-node' : null
- */
